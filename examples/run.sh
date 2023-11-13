f9c51208055a6131e297ac538c876464e26e2211
Prete <martin.prete@sanger.ac.uk>
2023-11-13 14:51:28 +0000

    Update run.sh
    Add Singularity to `PATH` and set `LSB_DEFAULT_USERGROUP` inside run.sh example script for those who haven't set it up in their profiles already.

--- examples/run.sh
+++ examples/run.sh
@@ -2,6 +2,12 @@
 
 set -eo pipefail
 
+# LSF group is set and visible for nextflow job submissions
+export LSB_DEFAULT_USERGROUP=<YOURGROUP>
+
+# add Singularity to the the path
+export PATH="/software/singularity/v3.10.0/bin:$PATH"
+
 #input file, CSV with irods metadata
 META="/path/to/samples.csv"
 
